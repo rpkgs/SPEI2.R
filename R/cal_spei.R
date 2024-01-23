01bcde7cba7923cf7d9ae0c765035e61d344811b
Dongdong Kong <kongdd@users.noreply.github.com>
2024-01-23 22:56:50 +0800

    improve spei

--- R/cal_spei.R
+++ R/cal_spei.R
@@ -4,46 +4,69 @@
 #' @importFrom lmomco pwm2lmom are.lmom.valid pwm.pp
 NULL
 
-par_nor = setNames(rep(NA, 2), c("mu", "sigma"))
-par_gam = setNames(rep(NA, 2), c("alpha", "beta"))
-par_pe3 = setNames(rep(NA, 3), c("mu", "sigma", "gamma"))
-par_glo = setNames(rep(NA, 3), c("xi", "alpha", "kappa"))
+last <- \(x) x[[length(x)]]
+
+# x -> cdf -> z
+qGamma <- function(x, coef, cdf = cdfgam) {
+  pze <- last(coef) # the probability of x <= 0
+  npar <- length(coef) - 1
+  cdf_res <- cdf(x, coef[1:npar]) # exclude nans
+  z <- qnorm(pze + (1 - pze) * cdf_res)
+  z[x <= 0] <- -Inf
+  z
+}
+
+qNormal <- function(x, coef, cdf = cdfnor) {
+  cdf(x, coef) |> qnorm()
+}
+
+qPearsonIII <- \(x, coef) qGamma(x, coef, cdf = cdfpe3)
+
+qLogLogistic <- \(x, coef) qNormal(x, coef, cdf = cdfglo)
+
+
+par_nor <- setNames(rep(NA, 2), c("mu", "sigma"))
+par_gam <- setNames(rep(NA, 3), c("alpha", "beta", "pzero"))
+par_pe3 <- setNames(rep(NA, 4), c("mu", "sigma", "gamma", "pzero"))
+par_glo <- setNames(rep(NA, 3), c("xi", "alpha", "kappa"))
 # par_gev = setNames(rep(NA, 3), c("xi", "alpha", "kappa"))
 
 .options <- list(
-  "Normal"       = list(pel = pelnor, par = parnor, cdf = cdfnor, param = par_nor),
-  "Gamma"        = list(pel = pelgam, par = pargam, cdf = cdfgam, param = par_gam),
-  "PearsonIII"   = list(pel = pelpe3, par = parpe3, cdf = cdfpe3, param = par_pe3),
-  "log-Logistic" = list(pel = pelglo, par = parglo, cdf = cdfglo, param = par_glo)
+  "Normal"       = list(pel = pelnor, par = parnor, inv = qNormal, param = par_nor),
+  "Gamma"        = list(pel = pelgam, par = pargam, inv = qGamma, param = par_gam),
+  "PearsonIII"   = list(pel = pelpe3, par = parpe3, inv = qPearsonIII, param = par_pe3),
+  "log-Logistic" = list(pel = pelglo, par = parglo, inv = qLogLogistic, param = par_glo)
 )
 
 #' cal_spei
 #'
-#' @param x A monthly time series of certain month
+#' @param x A monthly time series of certain month, which you want to calculate drought index.
+#' @param x_ref the reference time series to fit distribution
+#'
 #' @param distribution Distribution name, one of `Normal`, `Gamma`, `PearsonIII`, `log-Logistic`.
 #' @param fit Fitting method, one of `ub-pwm`, `pp-pwm`, `max-lik`.
 #' @param ... ignored
 #'
 #' @examples
 #' cal_spei(wb)
-#' cal_spei(wb, fit="max-lik")
+#' cal_spei(wb, fit = "max-lik")
 #' cal_spei(wb, distribution = "Normal")
 #' cal_spi(wb)
 #' @export
-cal_spei <- function(x, distribution = "log-Logistic", fit = "ub-pwm", ...) {
+cal_spei <- function(x, x_ref = x, distribution = "log-Logistic", fit = "ub-pwm", ...) {
   r <- list(z = x * NA, coef = .options[[distribution]]$param)
 
-  x.mon <- x[!is.na(x)]
-  if (distribution %in%  c("Gamma", "PearsonIII")) {    
+  x.mon <- x_ref[!is.na(x_ref)]
+  if (distribution %in% c("Gamma", "PearsonIII")) {
     pze <- sum(x.mon <= 0) / length(x.mon)
     x.mon <- x.mon[x.mon > 0]
   }
 
   # Get functions
   dist <- .options[[distribution]]
-  pel_lmom   <- dist$pel
+  pel_lmom <- dist$pel
   par_lmomco <- dist$par
-  cdf        <- dist$cdf
+  inv <- dist$inv
 
   # Fit distribution parameters
   if (length(x.mon) < 4) {
@@ -75,7 +98,7 @@ cal_spei <- function(x, distribution = "log-Logistic", fit = "ub-pwm", ...) {
   fortran_vec <- c(lmom$lambdas[1:2], lmom$ratios[3])
 
   # Calculate parameters based on distribution with `lmom`, then `lmomco`
-  f_params <- tryCatch(
+  coef <- tryCatch(
     pel_lmom(fortran_vec),
     error = function(e) {
       par_lmomco(lmom)$para
@@ -84,23 +107,16 @@ cal_spei <- function(x, distribution = "log-Logistic", fit = "ub-pwm", ...) {
 
   # Adjust if user chose `log-Logistic` and `max-lik`
   if (distribution == "log-Logistic" && fit == "max-lik") {
-    f_params <- parglo.maxlik(x.mon, f_params)$para
+    coef <- parglo.maxlik(x.mon, coef)$para
   }
+  if (distribution %in% c("Gamma", "PearsonIII")) coef["pzero"] <- pze
 
-  cdf_res <- cdf(x, f_params) # exclude nans
-  z <- qnorm(cdf_res)
-
-  # Adjust for `pze` if distribution is Gamma or PearsonIII
-  if (distribution %in%  c("Gamma", "PearsonIII")) {    
-    z <- qnorm(pze + (1 - pze) * cdf_res)
-    z[x <= 0] <- -Inf
-  }
-  list(z = z, coef = f_params)
+  z <- inv(x, coef)
+  list(z = z, coef = coef)
 }
-
 
 #' @rdname cal_spei
 #' @export
-cal_spi <- function(x, distribution = "Gamma", fit = "ub-pwm", ...) {
-  cal_spei(x, distribution, fit, ...)
+cal_spi <- function(x, x_ref = x, distribution = "Gamma", fit = "ub-pwm", ...) {
+  cal_spei(x, x_ref, distribution, fit, ...)
 }
